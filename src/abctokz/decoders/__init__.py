c8e9de92baf494d9b20b7b5ac2cc385cbca55809
augenblick06 <augenblickconsulting@gmail.com>
2026-03-13 13:58:57 +0100

    Update decoders __init__.py

--- src/abctokz/decoders/__init__.py
+++ src/abctokz/decoders/__init__.py
@@ -1,1 +1,13 @@
+# Augenblick — abctokz
+"""Decoder subpackage for abctokz."""
+
+from abctokz.decoders.base import Decoder
+from abctokz.decoders.subword_decoder import SubwordDecoder
+from abctokz.decoders.word_decoder import WordDecoder
+
+__all__ = [
+    "Decoder",
+    "SubwordDecoder",
+    "WordDecoder",
+]
 
